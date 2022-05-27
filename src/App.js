7e7b50f24d585313e073da02d3ad660751d7d307
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-27 07:24:31 -0300

    feat: Routes included in App file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Schedule from './pages/Schedule';
+import ChangeTask from './pages/ChangeTask';
+import ExcludeTask from './pages/ExcludeTask';
+import NotFound from './pages/NotFound';
+import CreateTask from './pages/CreateTask';
 
 const App = () => (
-  <Schedule />
+  <Routes>
+    <Route exact path="/" element={ <Schedule /> } />
+    <Route path="/changetask" element={ <ChangeTask /> } />
+    <Route path="/excludetask" element={ <ExcludeTask /> } />
+    <Route path="/createtask" element={ <CreateTask /> } />
+    <Route path="*" element={ <NotFound /> } />
+  </Routes>
 );
 
 export default App;
